a5f00d8ef08fbf0cbdc5fd3d2267ddefcf157b7b
Do Trem <dobisi13@gmail.com>
2025-04-10 23:02:09 +0300

    Add React app structure

---
+++ src/App.js
@@ -0,0 +1,1 @@
+import React from "react";function App(){return(<div className="App"><header className="App-header"><h1>Welcome to Chartrem</h1><p>Your chart creation tool</p></header></div>);}export default App;
